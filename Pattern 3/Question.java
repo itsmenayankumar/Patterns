7617aa7490b0e33e59dd0dbd87b6b4f4f615c743
Nayan_Yadav <nayank117@gmail.com>
2023-09-19 00:08:10 +0530

    task

---
+++ Pattern 3/Question.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Question {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n+1-i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
