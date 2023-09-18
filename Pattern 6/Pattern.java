2b1a4ec311f5d001196e449324344f4fae9ac871
Nayan_Yadav <nayank117@gmail.com>
2023-09-19 00:44:41 +0530

    pattern

---
+++ Pattern 6/Pattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Pattern {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i-1; j++) {
+                System.out.print("    ");
+            }
+            for(int j = n; j >= i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
